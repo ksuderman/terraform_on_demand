2aacc7dc6c15b5512d6c60e8d920f7fa40265104
Keith Suderman <suderman@jhu.edu>
2021-10-11 15:17:42 -0400

    Change location of generated comment file

--- output.tf
+++ output.tf
@@ -1,10 +1,10 @@
 locals {
-    key = pathexpand("~/.ssh/${var.key_pair}.pem")
+    key = var.key_pair
     ip = openstack_compute_floatingip_v2.ip.address
 }
 
 resource "local_file" "ipfile" {
     content = templatefile("./templates/cluster.tpl", { ip=local.ip, key=local.key})
-    filename = pathexpand("./comment.txt")
+    filename = "./comment.txt"
 }
 
