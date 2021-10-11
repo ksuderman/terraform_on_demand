aad56c793fc4c84d0459837fa1e066c50e2b8bbc
Keith Suderman <suderman@jhu.edu>
2021-10-11 14:38:22 -0400

    Write comment with IP addresses to a file.

--- output.tf
+++ output.tf
@@ -3,19 +3,8 @@ locals {
     ip = openstack_compute_floatingip_v2.ip.address
 }
 
-# output "ip_addresses" {
-#     count = var.num_nodes
-#     value = "${openstack_compute_instance_v2.nodes[count.index].name} = ${openstack_compute_floatingip_v2.floating_ips[count.index].address}"
-# } 
-
 resource "local_file" "ipfile" {
     content = templatefile("./templates/cluster.tpl", { ip=local.ip, key=local.key})
-    filename = pathexpand("~/.cluster/ghjobs")
+    filename = pathexpand("./comment.txt")
 }
 
-# resource "local_file" "inventories" {
-#     count = var.num_nodes
-#     content = templatefile("./templates/inventory.tpl", { ip=local.ips[count.index].address, key=local.key, name=local.nodes[count.index].name})
-#     filename = pathexpand("~/.inventories/bench${count.index+1}.ini")
-#     #filename = "/tmp/bench${count.index+1}.ini"
-# }
